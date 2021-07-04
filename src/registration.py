2cfca28584bd204adcca82fad902efc2cf63c6f0
Barnone <dongming.wei@sjtu.edu.cn>
2021-07-04 22:31:21 +0800

    Update registration.py

--- src/registration.py
+++ src/registration.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 """
 Created on Sat Jun 26 13:19:07 2021
-@author: shaon
+@author: Dongming Wei
 """
 import numpy as np
 import SimpleITK as sitk
 from matplotlib import pyplot as plt
 import cv2
 import sys
-import os
+from math import pi
 
 
 def rgb2gray(rgb):
@@ -29,19 +29,21 @@ def padding_zeros_2D(img, shape):
         
     return padded
 
-def pad_and_resize(img, padding=(600,600), output_dims=(256,256), interpolation='linear'):
-    img = padding_zeros_2D(img, padding)
+def pad_and_resize(img, padding=(30,10), output_dims=(256,256), interpolation='linear'):
+    
     if interpolation == 'linear':
-        img = cv2.resize(img, output_dims, interpolation=cv2.INTER_LINEAR)
+        img = cv2.resize(img, np.subtract(output_dims, padding), interpolation=cv2.INTER_LINEAR)
     elif interpolation == 'nearest':
-        img = cv2.resize(img, output_dims, interpolation=cv2.INTER_NEAREST)
+        img = cv2.resize(img, np.subtract(output_dims, padding), interpolation=cv2.INTER_NEAREST)
+        
+    img = padding_zeros_2D(img, padding)
         
     return img
     
 
 def preprocess(MR_dir='../MR/aaa0043/09-16-2000-PELVISPROSTATE-50407/4.000000-T2AXIALSMFOV-51544', 
                Pathology_dir='../Pathology/aaa0043D', Results_Folder='./',
-               output_dims=(256,256)):
+               output_dims=(256,256), mr_slice_id=12, padding=(30,10)):
     ###########
     # function: the preprocess function read the original MR and pathology images, and perform image masking,
     # cropping, padding, resizing, and intensity normalization
@@ -57,48 +59,45 @@ def preprocess(MR_dir='../MR/aaa0043/09-16-2000-PELVISPROSTATE-50407/4.000000-T2
     mr_img_data = sitk.GetArrayFromImage(mr_img)
     mr_label_img_data = sitk.GetArrayFromImage(mr_label_img)
     
-    mr_image_data_slice = mr_img_data[12,::-1,]
-    mr_label_img_data_slice = mr_label_img_data[12,:,:]
+    mr_image_data_slice = mr_img_data[mr_slice_id,::-1,]
+    mr_label_img_data_slice = mr_label_img_data[mr_slice_id,:,:]
     
     mr_masked = mr_image_data_slice * mr_label_img_data_slice
     index = np.where(mr_label_img_data_slice == 1)
-    mr_masked = mr_masked[index[0].min()-5 : index[0].max()+10, index[1].min()-5 : index[1].max()+10]
+    
     mr_masked = np.uint8(mr_masked.astype(np.float)/np.max(mr_masked)*255)
-    mr_masked = cv2.resize(mr_masked, output_dims, interpolation=cv2.INTER_LINEAR)
-    plt.imsave(Results_Folder + './MR_masked.png', mr_masked, cmap='gray')
+    mr_masked = mr_masked[index[0].min()-5 : index[0].max()+10, index[1].min()-5 : index[1].max()+10]
+    mr_masked = pad_and_resize(mr_masked,padding=padding)
     mr_masked = sitk.GetImageFromArray(mr_masked)
     sitk.WriteImage(mr_masked, Results_Folder + './MR_masked.nii.gz')
-    img = np.uint8(mr_label_img_data_slice.astype(np.float)/np.max(mr_label_img_data_slice)*255)
-    img = img[index[0].min()-5 : index[0].max()+5, index[1].min()-5 : index[1].max()+5]
     
-    img = cv2.resize(img, output_dims, interpolation=cv2.INTER_NEAREST)
+    img = np.uint8(mr_label_img_data_slice.astype(np.float)/np.max(mr_label_img_data_slice)*255)
+    img = img[index[0].min()-5 : index[0].max()+10, index[1].min()-5 : index[1].max()+10]
+    img = pad_and_resize(img, padding=padding)
     img = sitk.GetImageFromArray(img)
+    img.CopyInformation(mr_masked)
     sitk.WriteImage(img, Results_Folder + './MR_label.nii.gz')
     
     
     # read Pathology image
     Pathology = Pathology_dir + '/img.png'
     Pathology_label = Pathology_dir + '/label.png'
-    Pathology_ori = plt.imread(Pathology)
-    #Pathology_ori = padding_zeros_2D(Pathology_ori, (600, 600))
-    #Pathology_ori = cv2.resize(Pathology_ori, output_dims, interpolation=cv2.INTER_LINEAR)
-    Pathology_ori = pad_and_resize(Pathology_ori)
-    Pathology_data = rgb2gray(plt.imread(Pathology))
-    Pathology_label_data = rgb2gray(plt.imread(Pathology_label))
+    
+    Pathology_data = pad_and_resize(rgb2gray(plt.imread(Pathology)), output_dims=output_dims, padding=padding, interpolation='linear')
+    Pathology_label_data = pad_and_resize(rgb2gray(plt.imread(Pathology_label)), output_dims=output_dims, padding=padding, interpolation='nearest')
     Pathology_label_data[Pathology_label_data < Pathology_label_data.max()] = 0
-    Pathology_label_data[Pathology_label_data >0] = 1
+    Pathology_label_data[Pathology_label_data > 0] = 1
     
     Pathology_masked = Pathology_data * Pathology_label_data
-    Pathology_masked = pad_and_resize(Pathology_masked, output_dims=output_dims)
+    #Pathology_masked = pad_and_resize(Pathology_masked, output_dims=output_dims)
 
-    Pathology_label_data = pad_and_resize(Pathology_label_data, output_dims=output_dims, interpolation='nearest')
+    #Pathology_label_data = pad_and_resize(Pathology_label_data, output_dims=output_dims, interpolation='nearest')
     
+    Pathology_ori = pad_and_resize(plt.imread(Pathology), padding=padding)
     for i in range(np.shape(Pathology_ori)[2]):
         P_ori_img = sitk.GetImageFromArray(Pathology_ori[:,:,i] * Pathology_label_data)
-        #P_ori_img.CopyInformation(img)
         sitk.WriteImage(P_ori_img, Results_Folder + './Pathology_ori_' + str(i) + '.nii.gz')
     
-    plt.imsave(Results_Folder + './Pathology_masked.png', Pathology_masked, cmap='gray')
     Pathology_masked = sitk.GetImageFromArray(np.uint8(Pathology_masked.astype(np.float)/np.max(Pathology_masked)*255))
     Pathology_masked.CopyInformation(mr_masked)
     sitk.WriteImage(Pathology_masked, Results_Folder + './Pathology_masked.nii.gz')
@@ -106,7 +105,10 @@ def preprocess(MR_dir='../MR/aaa0043/09-16-2000-PELVISPROSTATE-50407/4.000000-T2
     p_img.CopyInformation(img)
     sitk.WriteImage(p_img, Results_Folder + './Pathology_label.nii.gz')
 
-def postprocess(warped0='./warped_Pathology_ori_0.nii.gz',warped1='./warped_Pathology_ori_1.nii.gz',warped2='./warped_Pathology_ori_2.nii.gz'):
+def postprocess(Dir='./'):
+    warped0= Dir + '/warped_pa_0.nii.gz'
+    warped1= Dir + '/warped_pa_1.nii.gz'
+    warped2= Dir + '/warped_pa_2.nii.gz'
     warped0 = sitk.GetArrayFromImage(sitk.ReadImage(warped0))
     warped1 = sitk.GetArrayFromImage(sitk.ReadImage(warped1))
     warped2 = sitk.GetArrayFromImage(sitk.ReadImage(warped2))
@@ -114,7 +116,7 @@ def postprocess(warped0='./warped_Pathology_ori_0.nii.gz',warped1='./warped_Path
     warped1 = np.expand_dims(warped1, axis=-1)
     warped2 = np.expand_dims(warped2, axis=-1)
     a = np.concatenate([warped0,warped1,warped2], axis=-1)
-    plt.imsave('./warepd_pa.png', a)
+    plt.imsave(Dir + '/warepd_pa.png', a)
     
     return a
 
@@ -149,7 +151,7 @@ class displacement_registration():
         R.SetShrinkFactorsPerLevel([3, 2, 1])
         R.SetSmoothingSigmasPerLevel([2, 1, 1])
         
-        R.SetMetricAsJointHistogramMutualInformation(20)
+        R.SetMetricAsJointHistogramMutualInformation(5)#20
         R.MetricUseFixedImageGradientFilterOff()
         
         R.SetOptimizerAsGradientDescent(learningRate=1.0,
@@ -183,15 +185,15 @@ class displacement_registration():
         R.SetMovingInitialTransform(outTx1)
         R.SetInitialTransform(displacementTx, inPlace=True)
         
-        R.SetMetricAsANTSNeighborhoodCorrelation(4)
+        R.SetMetricAsANTSNeighborhoodCorrelation(1)#4
         R.MetricUseFixedImageGradientFilterOff()
         
         R.SetShrinkFactorsPerLevel([3, 2, 1])
-        R.SetSmoothingSigmasPerLevel([2, 1, 1])
+        R.SetSmoothingSigmasPerLevel([1, 1, 1])# 2,1,1
         
         R.SetOptimizerScalesFromPhysicalShift()
         R.SetOptimizerAsGradientDescent(learningRate=1,
-                                        numberOfIterations=300,
+                                        numberOfIterations=500,
                                         estimateLearningRate=R.EachIteration)
         
         R.Execute(fixed, moving)
@@ -208,7 +210,7 @@ class displacement_registration():
         resampler = sitk.ResampleImageFilter()
         resampler.SetReferenceImage(fixed)
         resampler.SetInterpolator(sitk.sitkLinear)
-        resampler.SetDefaultPixelValue(100)
+        resampler.SetDefaultPixelValue(0)
         resampler.SetTransform(compositeTx)
         
         out = resampler.Execute(moving)
@@ -259,8 +261,17 @@ class demons_registration():
         out = resampler.Execute(moving)
         sitk.WriteImage(out, 'itk_demons_out.nii.gz')
 
-
 class bspline_registration():
+    def command_iteration_affine(self, method):
+        if (method.GetOptimizerIteration() == 0):
+            print(f"\tLevel: {method.GetCurrentLevel()}")
+            print(f"\tScales: {method.GetOptimizerScales()}")
+        print(f"#{method.GetOptimizerIteration()}")
+        print(f"\tMetric Value: {method.GetMetricValue():10.5f}")
+        print(f"\tLearningRate: {method.GetOptimizerLearningRate():10.5f}")
+        if (method.GetOptimizerConvergenceValue() != sys.float_info.max):
+            print(f"\tConvergence Value: {method.GetOptimizerConvergenceValue():.5e}")
+    
     def command_iteration(self, method, bspline_transform):
         if method.GetOptimizerIteration() == 0:
             # The BSpline is resized before the first optimizer
@@ -269,6 +280,10 @@ class bspline_registration():
             print(bspline_transform)
     
         print(f"{method.GetOptimizerIteration():3} = {method.GetMetricValue():10.5f}")
+    
+    def command_multiresolution_iteration(self, method):
+        print(f"\tStop Condition: {method.GetOptimizerStopConditionDescription()}")
+        print("============= Resolution Change =============")
     
     
     def command_multi_iteration(self, R):
@@ -281,6 +296,42 @@ class bspline_registration():
             print(f" Metric value: {R.GetMetricValue()}")
     
         print("--------- Resolution Changing ---------")
+    
+    def affine(self, MOVING, FIXED, ind='affine'):
+        fixed = sitk.ReadImage(FIXED, sitk.sitkFloat32)
+    
+        moving = sitk.ReadImage(MOVING, sitk.sitkFloat32)
+        
+        
+        if ind == "affine":
+            initialTx = sitk.CenteredTransformInitializer(fixed, moving, sitk.AffineTransform(fixed.GetDimension()))
+        else:
+            initialTx = sitk.CenteredTransformInitializer(fixed, moving, sitk.Euler2DTransform(), sitk.CenteredTransformInitializerFilter.GEOMETRY)
+        
+        R = sitk.ImageRegistrationMethod()
+        
+        R.SetShrinkFactorsPerLevel([3, 2, 1])
+        R.SetSmoothingSigmasPerLevel([2, 1, 1])#2,1,1
+        
+        R.SetMetricAsJointHistogramMutualInformation()#20
+        R.MetricUseFixedImageGradientFilterOff()
+        
+        R.SetOptimizerAsGradientDescent(learningRate=1.0,
+                                        numberOfIterations=100,
+                                        estimateLearningRate=R.EachIteration)
+        R.SetOptimizerScalesFromPhysicalShift()
+        
+        R.SetInitialTransform(initialTx)
+        
+        R.SetInterpolator(sitk.sitkLinear)
+        
+        R.AddCommand(sitk.sitkIterationEvent, lambda: self.command_iteration_affine(R))
+        R.AddCommand(sitk.sitkMultiResolutionIterationEvent,
+                     lambda: self.command_multiresolution_iteration(R))
+        
+        outTx1 = R.Execute(fixed, moving)
+        
+        return outTx1
         
     def reg(self, MOVING='./Pathology_masked.nii.gz', FIXED='./MR_masked.nii.gz', Transform='./trans.hdf'):
     
@@ -288,21 +339,29 @@ class bspline_registration():
         
         moving = sitk.ReadImage(MOVING, sitk.sitkFloat32)
         
-        transformDomainMeshSize = [2] * fixed.GetDimension()
+        transformDomainMeshSize = [1] * fixed.GetDimension()
         tx = sitk.BSplineTransformInitializer(fixed,
                                               transformDomainMeshSize)
         
         print(f"Initial Number of Parameters: {tx.GetNumberOfParameters()}")
         
+        
         R = sitk.ImageRegistrationMethod()
+        
         R.SetMetricAsJointHistogramMutualInformation()
         
-        R.SetOptimizerAsGradientDescentLineSearch(5.0,
-                                                  100,
+        R.SetOptimizerAsGradientDescentLineSearch(5.0, #5.0
+                                                  100, #100
                                                   convergenceMinimumValue=1e-4,
-                                                  convergenceWindowSize=5)
+                                                  convergenceWindowSize=5)#5
         
         R.SetInterpolator(sitk.sitkLinear)
+        
+        # affine transform
+        affine = self.affine(MOVING, FIXED, ind='rotate')
+        
+        
+        R.SetMovingInitialTransform(affine)
         
         R.SetInitialTransformAsBSpline(tx,
                                        inPlace=True,
@@ -323,29 +382,90 @@ class bspline_registration():
         print(f" Iteration: {R.GetOptimizerIteration()}")
         print(f" Metric value: {R.GetMetricValue()}")
         
-        sitk.WriteTransform(outTx, Transform)
+        compositeTx = sitk.CompositeTransform([affine, outTx])
+        sitk.WriteTransform(compositeTx, Transform)
         
         resampler = sitk.ResampleImageFilter()
         resampler.SetReferenceImage(fixed)
         resampler.SetInterpolator(sitk.sitkLinear)
-        resampler.SetDefaultPixelValue(100)
-        resampler.SetTransform(outTx)
+        resampler.SetDefaultPixelValue(0)
+        resampler.SetTransform(compositeTx)
     
         out = resampler.Execute(moving)
         sitk.WriteImage(out, 'itk_bspline_out.nii.gz')
         
+        warper(MOVING='./Pathology_label.nii.gz', FIXED='./MR_label.nii.gz', Transform=Transform)
+
+
+def warper(MOVING, FIXED, Transform, SAVING = 'warped_label.nii.gz', Interpolation = sitk.sitkNearestNeighbor):
+    moving = sitk.ReadImage(MOVING, sitk.sitkFloat32)
+    fixed = sitk.ReadImage(FIXED, sitk.sitkFloat32)
+    transform = sitk.ReadTransform(Transform)
+    resampler = sitk.ResampleImageFilter()
+    resampler.SetReferenceImage(fixed)
+    resampler.SetInterpolator(Interpolation)
+    resampler.SetDefaultPixelValue(0)
+    resampler.SetTransform(transform)
+    out = resampler.Execute(moving)
+    sitk.WriteImage(out, SAVING)
+    
+def dice(FIXED='./MR_label.nii.gz', MOVED='./warped_label.nii.gz',label=255):
+    label=int(label)
+    if label==0:
+        fixed = sitk.ReadImage(FIXED)
+        moved = sitk.ReadImage(MOVED)
+        
+        fix_data = sitk.GetArrayFromImage(fixed)
+        moved_data = sitk.GetArrayFromImage(moved)
+        MO = np.sum(moved_data[(fix_data>label) * (moved_data>label)])*2.0/(np.sum(fix_data[fix_data>label])
+                + np.sum(moved_data[moved_data>label]))
+        print("Dice: %.2f" % (100*MO) + "%")
+        return MO
+    else:
+        label=int(label)
+        fixed = sitk.ReadImage(FIXED)
+        moved = sitk.ReadImage(MOVED)
+        
+        fix_data = sitk.GetArrayFromImage(fixed)
+        moved_data = sitk.GetArrayFromImage(moved)
+        if np.sum(fix_data[fix_data==label]) + np.sum(moved_data[moved_data==label]) ==0:
+            print("[Warning]: No this label!")
+        else:
+            MO = np.sum(moved_data[(fix_data==label) * (moved_data==label)])*2.0/(np.sum(fix_data[fix_data==label])
+                    + np.sum(moved_data[moved_data==label]))
+            print("Dice: %.2f" % (100*MO) + "%")
+            return MO
+        
+def Hausdorff_distance(FIXED='./MR_label.nii.gz', MOVED='./warped_label.nii.gz'):
+    
+    fix = sitk.ReadImage(FIXED, sitk.sitkFloat64)
+    moved = sitk.ReadImage(MOVED, sitk.sitkFloat64)
+    
+    hausdorff_distance_filter = sitk.HausdorffDistanceImageFilter()
+
+    # Use reference1, larger inner annulus radius, the surface based computation
+    # has a smaller difference. 
+    hausdorff_distance_filter.Execute(fix, moved)
+    print('HausdorffDistanceImageFilter result (reference1-segmentation): %.2f ' % 
+          hausdorff_distance_filter.GetHausdorffDistance())
             
 if __name__ == '__main__':
+    Saving_Dir='../Results/aa0051'
     if len(sys.argv) < 2:
-        #preprocess(MR_dir='/home/winter/Downloads/MR-Pathology/MR/aaa0051/07-02-2000-PELVISPROSTATE-97855/4.000000-T2 AXIAL SM FOV-36207',
-         #          Pathology_dir='/home/winter/Downloads/MR-Pathology/Pathology/aaa0051E')
+        preprocess(MR_dir='../MR/aaa0051/07-02-2000-PELVISPROSTATE-97855/4.000000-T2 AXIAL SM FOV-36207', Pathology_dir='../Pathology/aaa0051E', padding=(32,16))
+        #preprocess(padding=(6,6))
         # We have several options for the registration model
         # 1. displacement_registration
         # 2. demons_registration
         # 3. bspline_registration
         reg = bspline_registration()
         reg.reg()
+        dice(FIXED= Saving_Dir + '/ITK-BSpline/MR_label.nii.gz', MOVED=Saving_Dir'/ANTs/warped_pathology_label.nii.gz')
+        Hausdorff_distance(FIXED= Saving_Dir + '/ITK-BSpline/MR_label.nii.gz', MOVED= Saving_Dir + '/ANTs/warped_pathology_label.nii.gz')
+        #postprocess('../Results/aa0051/ANTs')
+    
     elif sys.argv[1] == 'pre':
-        preprocess()
+        preprocess(MR_dir='../MR/aaa0051/07-02-2000-PELVISPROSTATE-97855/4.000000-T2 AXIAL SM FOV-36207',
+                   Pathology_dir='../Pathology/aaa0051E')
     elif sys.argv[1] == 'post':
         postprocess()
